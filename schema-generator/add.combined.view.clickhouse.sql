cb4033a7bfaddfa6d761d0cc0dd52bb88de9baa0
Dhruv Baliyan <dhruvbaliyan1001@gmail.com>
2024-05-02 10:36:33 +0530

    feat: add mic_tap_view MV

--- schema-generator/add.combined.view.clickhouse.sql
+++ schema-generator/add.combined.view.clickhouse.sql
@@ -34,6 +34,7 @@ SELECT
     e2.phoneNumber AS phoneNumber,
     e2.district AS district,
     e2.block AS block,
+    e2.streamStartLatency as streamStartLatency,
     e3.getUserHistoryLatency AS getUserHistoryLatency,
     e4.getNeuralCoreferenceLatency AS getNeuralCoreferenceLatency,
     e5.classifyQuestionLatency AS classifyQuestionLatency,
@@ -81,6 +82,7 @@ FROM
             maxIf(spellCorrectedText, eventId = 'E002') AS spellCorrectedText,
             maxIf(text, eventId = 'E032') AS query,
             maxIf(timestamp, eventId = 'E017') AS responseAt,
+            maxIf(streamStartLatency, eventId = 'E012') as streamStartLatency,
             maxIf(
                 prompt, 
                 eventId = 'E012'
@@ -263,3 +265,16 @@ FROM
         GROUP BY
             messageId
     ) AS e12 ON e1.messageId = e12.messageId;
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS mic_tap_view
+REFRESH EVERY 5 SECONDS 
+ENGINE = MergeTree
+ORDER BY sessionId 
+SETTINGS allow_nullable_key = 1 AS
+SELECT
+    sessionId,
+    COUNTIf(eventId = 'E044') AS count
+FROM
+    event
+GROUP BY
+    sessionId;
